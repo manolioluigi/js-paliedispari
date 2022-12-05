35736824786cc291ce5b4b2420ea06644b1f29bb
manolioluigi <luigi.manolio1999@gmail.com>
2022-12-05 17:21:49 +0100

    script completato

--- js/script.js
+++ js/script.js
@@ -17,9 +17,11 @@ function palindroma(parola){
     //Controlliamo che siano uguali
     if (parola == reverseParola){
         console.log("E' palindroma");
+        document.getElementById('risultato-parola').innerHTML = "La parola è palindroma";
     }
     else{
         console.log("Non è palindroma");
+        document.getElementById('risultato-parola').innerHTML = "La parola non è palindroma";
     }
 
 }
@@ -36,5 +38,53 @@ bottone.addEventListener(`click`, function(){
     //Eseguiamo la funzione
     palindroma(parola);
 
+}
+);
+
+//2
+
+//Dichiariamo le variabili
+let numeroUtente, sceltaUtente, numeroRandom, risultato;
+
+//Creiamo la funzione per generare un numero random;
+
+function generatoreRandom(){
+    numeroRandom = Math.floor(Math.random() * 5 + 1);
+    console.log(numeroRandom);
+    return numeroRandom;
+}
+
+//Creiamo la funzione per stabilire se la somma dei numeri è pari o dispari
+
+function sommaNumeri(){
+    
+    if((numeroUtente + numeroRandom) % 2 == 0){
+        risultato = "pari";
+    }
+    else{
+        risultato = "dispari";
+    }
+
+    return risultato;
+}
+
+bottone = document.getElementById("bottone-PD");
+bottone.addEventListener(`click`, function(){
+
+//Recuperiamo i valori dell'utente
+numeroUtente = parseInt(document.getElementById('numero').value)
+console.log(numeroUtente);
+sceltaUtente = document.getElementById('pari-dispari').value.toLowerCase();
+
+
+generatoreRandom(numeroRandom);
+sommaNumeri(numeroUtente, numeroRandom);
+
+if(risultato == sceltaUtente){
+    document.getElementById('risultato').innerHTML = "Hai vinto!"
+}else{
+    document.getElementById('risultato').innerHTML = "Hai perso!"
+}
+console.log(sceltaUtente);
 }
 );
